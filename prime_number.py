b8f8b67210817bb855bcd34b841d2781953a779c
Atharva <jadhavatharva20@gmail.com>
2025-11-26 00:22:01 +0530

    Optimised code

--- prime_number.py
+++ prime_number.py
@@ -1,14 +1,32 @@
+import math
+
 def prime(n):
-    if n < 1:
+    if n <= 1:
         return "Not Prime"
     
     for i in range(2 , n):
-        if i%i == 0:
+        if n % i == 0:
             return "Not Prime"
     
     return "Prime"
 
-print(prime(5))
+
+#optimised code
+
+def isprime(n):
+    if n <= 1:
+        return "Not Prime"
+
+    for i in range(2, int(math.sqrt(n)+1)):
+        if n % 2 == 0:
+            return "Not Prime"
+
+    return "Prime"
+
+def main():
+    print(prime(5))
+    print(isprime(3))
+
+main()
 
 
-#
