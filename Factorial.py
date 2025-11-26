3add3798b91f602d444f7b09b35f9d3ea7f8c0a0
Atharva <jadhavatharva20@gmail.com>
2025-11-26 23:16:19 +0530

    Factorial with recursion

--- Factorial.py
+++ Factorial.py
@@ -1,11 +1,18 @@
-def fact(n):
+def fact_loop(n):
     result = 1
 
     for i in range(1,n+1):
         result *= i 
-    print(result)
+    return result
+
+def fact_recursion(n):
+    if n==0 or n==1:
+        return 1
+    return n * fact_recursion(n-1)
 
 def main():
-    fact(6)
+    print(fact_loop(6))
+    print(fact_recursion(3))
 
-main()
+if __name__=='__main__':
+    main()
