692cef8605a29447364d345995f6d242445fe422
Atharva <jadhavatharva20@gmail.com>
2025-11-26 22:57:37 +0530

    count vowels and consonants

---
+++ count_vowels.py
@@ -0,0 +1,21 @@
+def count_vowels_consonants(text):
+    vowels = 'aeiouAEIOU'
+    vowel_count = 0
+    consonant_count = 0
+
+    for char in text:
+        if char.isalpha():
+            if char in vowels:
+                vowel_count += 1
+            else:
+                consonant_count += 1
+
+    return vowel_count, consonant_count
+
+def main():
+    text = input("Enter the text:")
+    vowels, consonants = count_vowels_consonants(text)
+    print("vowels:", vowels)
+    print("consonants:", consonants)
+
+main()
