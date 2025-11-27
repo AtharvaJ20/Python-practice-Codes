8beb000b7fd7b794b061a5c39eb5165d06670e7c
Atharva <jadhavatharva20@gmail.com>
2025-11-27 23:52:35 +0530

    reverse string using built in function

--- reverse_string.py
+++ reverse_string.py
@@ -5,8 +5,14 @@ def loop(text):
         rev = char + rev
     return rev
 
+def built_in(text):
+    rev = "".join(reversed(text))
+    return rev
+
+
 def main():
     print(loop("Hello"))
+    print(built_in("Hello"))
 
 if __name__=='__main__':
     main()
